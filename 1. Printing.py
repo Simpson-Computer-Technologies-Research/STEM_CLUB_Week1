02eb7e0ef12a5ce079f337414cf5611b8d5cff17
Tristan <75189508+realTristan@users.noreply.github.com>
2022-11-07 11:01:12 -0500

    Create 1. Printing.py

---
+++ 1. Printing.py
@@ -0,0 +1,17 @@
+
+# //////////////
+# // Printing //
+# //////////////
+
+
+# // To print something to the terminal
+print("Hello World!")
+
+print("tristan")
+
+# // Print a number to the terminal
+print(100)
+
+
+# // Output 1: Hello World!
+# // Output 2: 100
