f6c2411e13745bd36f26ece68f9c3b509e3b3967
Tristan <75189508+realTristan@users.noreply.github.com>
2022-11-07 11:01:59 -0500

    Create 3. User Input.py

---
+++ 3. User Input.py
@@ -0,0 +1,23 @@
+# ////////////////////////
+# //    Input/Output    //
+# ////////////////////////
+
+
+# // User Input
+string_input = input("Enter String: ")
+
+
+# // Print only the variable
+print(string_input)
+
+
+# // Printing A Variable BASIC
+print("Your String Input Was: " + string_input)
+
+
+# // Wrap the input() with int()
+integer_input = int( input("Enter Number: ") )
+
+
+# // Printing A Variable FORMATTING
+print(f"Your Number Input Was: {integer_input}")
