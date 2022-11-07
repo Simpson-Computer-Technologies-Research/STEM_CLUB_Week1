f7bc50d357ba7c3e5bdc1ceafb1e87236061c856
Tristan <75189508+realTristan@users.noreply.github.com>
2022-11-07 11:02:49 -0500

    Create 6. Final Project Example.py

---
+++ 6. Final Project Example.py
@@ -0,0 +1,17 @@
+
+
+num = int(input("Enter a number: "))
+
+
+# // if number is equal to 9, you win!
+
+if num > 10:
+  print("Too High!")
+  
+elif num < 9:
+  print("Too low!")
+  
+else:
+  print("Yay! You win!")
+
+
