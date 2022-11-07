aeb4e1797403cde613f8c2c8843060825d216a25
Tristan <75189508+realTristan@users.noreply.github.com>
2022-11-07 11:01:29 -0500

    Create 2. Variables.py

---
+++ 2. Variables.py
@@ -0,0 +1,40 @@
+
+
+# ////////////////////////
+# //   Variable Types   //
+# ////////////////////////
+
+
+# String
+# Example: “Value”
+string_variable = "value"
+
+
+# Integer
+# Example: 0, 1, 2, 3, 4… and so on
+integer_variable = 1
+
+
+# Boolean
+# Example: True, False
+boolean_variable1 = True
+boolean_variable2 = False
+
+
+# Float
+# Example: 1.1, 1.2, 4.3, 9.10201… and so on
+float_variable = 1
+
+
+# List
+# Example: [1, 2, 3, 4, 5]
+list_variable = [1, 2, 3, 4, 5]
+
+
+# Dictionary
+# Example: {key: value}
+dictionary_variable = {
+  "key": "value"
+}
+
+# and more!
