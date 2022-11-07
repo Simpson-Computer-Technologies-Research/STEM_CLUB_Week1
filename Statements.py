2d298fc8b8974f3b8a65ed97ef362dfbbe18833f
Tristan <75189508+realTristan@users.noreply.github.com>
2022-11-07 11:02:29 -0500

    Create Statements.py

---
+++ Statements.py
@@ -0,0 +1,42 @@
+
+# //////////////////////////
+# //      Statements      //
+# //////////////////////////
+
+
+# // Declare a variable
+my_name = "Marcus"
+
+
+# // Utilize statements to determine what to print to the screen
+if my_name == "Tristan":
+  print("My name is Tristan")
+
+elif my_name == "Daniel":
+  print("My name is Daniel")
+
+else:
+  print("My name is unknown!")
+
+  
+
+# // Utilizing booleans
+my_name_is_tristan = True
+
+
+# // Utilize the if statements
+if my_name_is_tristan == True:
+  print("True")
+
+elif my_name_is_tristan == False:
+  print("False")
+
+
+  
+# // A better, cleaner, faster and easier way
+# // to declare the statement above is as so
+if my_name_is_tristan:
+  print("True")
+
+elif not my_name_is_tristan:
+  print("False")
