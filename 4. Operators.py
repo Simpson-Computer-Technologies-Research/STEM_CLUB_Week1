ed0298bc0a4cdbe5b7f1b5bcd0ae32f2149a896f
Tristan <75189508+realTristan@users.noreply.github.com>
2022-11-07 11:02:15 -0500

    Create 4. Operators.py

---
+++ 4. Operators.py
@@ -0,0 +1,96 @@
+# /////////////////
+# //  Operators  //
+# /////////////////
+
+#   ==    !=    >     <     >=     
+#   <=    +     -     -=    +=
+
+
+# // Variable 1 equals 2
+var_1 = 2
+
+# // Variable 2 equals 1
+var_2 = 1
+
+
+
+# // Add to a variable
+#
+# +
+#
+var_2 + 1
+
+
+# // Add from a variable and set value
+#
+# +=
+#
+var_1 += 1
+
+var_1 = var_1 + 1
+
+
+
+# // Remove from a variable
+#
+# -
+#
+var_1 - 1
+
+
+
+# // Remove from variable and set value
+#
+# -=
+#
+var_1 -= 1
+
+var_1 = var_1 - 1
+
+
+
+# // Variable1 is equal to Variable2
+#
+#  ==
+#
+var_1 == var_2
+
+
+
+# // Variable1 is not equal to Variable2
+#
+#  !=
+#
+var_1 != var_2
+
+
+
+# // Variable1 is greater than Variable2
+#
+#  >
+#
+var_1 > var_2
+
+
+
+# // Variable1 is less than Variable2
+#
+#  <
+#
+var_1 < var_2
+
+
+
+# // Variable1 is greater than or equal to Variable2
+#
+#  >=
+#
+var_1 >= var_2
+
+
+
+# // Variable1 is less than or equal to Variable2
+#
+#  <=
+#
+var_1 <= var_2
